a0a8c0b24b61bc6ecdad1ff72520ef84ec2eb567
fukamachi <e.arrows@gmail.com>
2011-07-06 17:33:24 +0900

    prepared for unit testing.

--- config/test.pl
+++ config/test.pl
@@ -1,10 +1,13 @@
 +{
-    'DBI' => [
-        'dbi:SQLite:dbname=test.db',
-        '',
-        '',
-        +{
+    'Teng' => {
+        dsn => 'dbi:SQLite:dbname=test.db',
+        username => '',
+        password => '',
+        connect_options => +{
             sqlite_unicode => 1,
-        }
-    ],
+        },
+    },
+    'Text::Xslate' => {
+        path => ['tmpl/'],
+    },
 };
--- t/00_compile.t
+++ t/00_compile.t
@@ -6,6 +6,13 @@ use_ok $_ for qw(
     Gotumda
     Gotumda::Web
     Gotumda::Web::Dispatcher
+    Gotumda::API
+    Gotumda::API::Dispatcher
+    Gotumda::DB
+    Gotumda::DB::Schema
+    Gotumda::DB::Row::Task
+    Gotumda::DB::Row::User
+    Gotumda::DB::Row::WatchProject
 );
 
 done_testing;
