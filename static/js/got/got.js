ebf4c127a8e0332e702b1a7cde33bc70ed990c66
fukamachi <e.arrows@gmail.com>
2011-07-10 07:57:40 +0900

    added JS root namespace 'got'.

---
+++ static/js/got/got.js
@@ -0,0 +1,14 @@
+// Copyright 2011 Eitarow Fukamachi. All rights reserved.
+
+/**
+ * @fileoverview Root package of Gotumda.
+ *
+ * @author e.arrows@gmail.com (Eitarow Fukamachi)
+ */
+
+goog.provide('got');
+
+goog.require('got.app.PC');
+goog.require('got.app.Project');
+goog.require('got.app.Tasks');
+goog.require('got.app.Timeline');
