3f615e0ccc99992faa44330e1e4b10d41a1ac350
fukamachi <e.arrows@gmail.com>
2011-07-12 08:14:58 +0900

    changed to order by created_at.

--- lib/Gotumda/API/Dispatcher.pm
+++ lib/Gotumda/API/Dispatcher.pm
@@ -7,7 +7,7 @@ get '/all-tasks.json' => sub {
     my ($c) = @_;
     my $iter = $c->db->search(
         task => {},
-        { order_by => { updated_at => 'ASC' } },
+        { order_by => { created_at => 'ASC' } },
     );
 
     return $c->render_json( [ map { $_->to_hashref } $iter->all ] );
